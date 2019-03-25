41fe930f92b1f0c9e1399665a79460274980e5b1
hingbong <hingbonglo@gmail.com>
2019-03-25 09:39:33 +0800

    login function

--- src/main/java/com/webserver/core/User.java
+++ src/main/java/com/webserver/core/User.java
@@ -9,57 +9,71 @@ import java.util.Objects;
 
 public class User implements Serializable {
 
-  private static final long serialVersionUID = 7645096949777878404L;
-  private String username;
-  private String password;
+    private static final long serialVersionUID = 7645096949777878404L;
+    private String username;
+    private String password;
 
-  public User(String username, String password) {
-    this.username = username;
-    this.password = password;
-  }
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 
-  public static boolean newUser(User user) throws IOException {
-    RandomAccessFile raf = new RandomAccessFile("user.dat", "rw");
-    byte[] data = new byte[32];
-    while (raf.read(data) != -1) {
-      if (new String(data, StandardCharsets.UTF_8).trim().equals(user.username)) {
+    public static boolean newUser(User user) throws IOException {
+        RandomAccessFile raf = new RandomAccessFile("user.dat", "rw");
+        byte[] data = new byte[32];
+        while (raf.read(data) != -1) {
+            if (new String(data, StandardCharsets.UTF_8).trim().equals(user.username)) {
+                return false;
+            }
+            raf.skipBytes(32);
+        }
+        raf.seek(raf.length());
+        byte[] usernameData = user.username.getBytes(StandardCharsets.UTF_8);
+        usernameData = Arrays.copyOf(usernameData, 32);
+        raf.write(usernameData);
+        byte[] passwordData = user.password.getBytes(StandardCharsets.UTF_8);
+        passwordData = Arrays.copyOf(passwordData, 32);
+        raf.write(passwordData);
+        return true;
+    }
+
+    public static boolean verifyUser(User user) throws IOException {
+        RandomAccessFile raf = new RandomAccessFile("user.dat", "r");
+        byte[] data = new byte[32];
+        while ((raf.read(data) != -1)) {
+            if (new String(data, StandardCharsets.UTF_8).trim().equals(user.username)) {
+                raf.read(data);
+                if (new String(data, StandardCharsets.UTF_8).trim().equals(user.password)) {
+                    return true;
+                }
+            }
+        }
         return false;
-      }
-      raf.skipBytes(32);
     }
-    raf.seek(raf.length());
-    byte[] usernameData = user.username.getBytes(StandardCharsets.UTF_8);
-    usernameData = Arrays.copyOf(usernameData, 32);
-    raf.write(usernameData);
-    byte[] passwordData = user.password.getBytes(StandardCharsets.UTF_8);
-    passwordData = Arrays.copyOf(passwordData, 32);
-    raf.write(passwordData);
-    return true;
-  }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof User)) {
+            return false;
+        }
+        User user = (User) o;
+        return username.equals(user.username) &&
+                password.equals(user.password);
     }
-    if (!(o instanceof User)) {
-      return false;
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, password);
     }
-    User user = (User) o;
-    return username.equals(user.username) &&
-        password.equals(user.password);
-  }
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(username, password);
-  }
-
-  @Override
-  public String toString() {
-    return "User{" +
-        "username='" + username + '\'' +
-        ", password='" + password + '\'' +
-        '}';
-  }
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/webserver/core/http/Request.java
+++ src/main/java/com/webserver/core/http/Request.java
@@ -1,6 +1,7 @@
 package com.webserver.core.http;
 
 import com.webserver.core.User;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URLDecoder;
@@ -9,164 +10,175 @@ import java.util.HashMap;
 
 public class Request {
 
-  private final InputStream in;
-  private HashMap<String, String> httpHeader;
+    private final InputStream in;
+    private HashMap<String, String> httpHeader;
+    private HashMap<String, String> parameters = new HashMap<>();
+    private String url;
+    private String requestLine;
+    private String protocol;
+    private String method;
+    private String requestURI;
+    private String queryString;
 
-  private String url;
-  private String requestLine;
-  private String protocol;
-  private String method;
-  private String requestURI;
-  private String queryString;
-
-  public Request(InputStream in) {
-    this.in = in;
-  }
-
-  public void start() {
-    String message = getMessage();
-    if (message == null) {
-      return;
+    public Request(InputStream in) {
+        this.in = in;
     }
-    setRequestLine(message);
-    parseURL();
-    parseURI();
-    parseParam();
-    storeHeader(message);
-    User user = postUser(message);
-    boolean isNewUser = false;
-    if (user != null) {
-      if (url.contains("reg")) {
+
+    public void start() {
+        String message = getMessage();
+        if (message == null) {
+            return;
+        }
+        setRequestLine(message);
+        parseURL();
+        parseURI();
+        parseParam();
+        storeHeader(message);
+        User user = postUser(message);
+        if (user != null) {
+            if (url.contains("reg")) {
+                boolean isNewUser = false;
+                try {
+                    isNewUser = User.newUser(user);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if (isNewUser) {
+                    System.out.println("user:" + user);
+                    requestURI = "user/reg_successfully.html";
+                } else {
+                    requestURI = "user/reg_failed.html";
+                    System.out.println("NOT Ａ NEW USER");
+                }
+            }
+            if (url.contains("index")) {
+                boolean isLoginOk = false;
+                try {
+                    isLoginOk = User.verifyUser(user);
+                } catch (IOException e) {
+                    requestURI = "user/login_failed.html";
+                    e.printStackTrace();
+                }
+                if (isLoginOk) {
+                    requestURI = "user/login_successfully.html";
+                } else requestURI = "user/login_failed.html";
+            }
+        }
+        System.out.println(requestURI);
+        System.out.println(url);
+    }
+
+    String getMessage() {
+        // 解析uri
+        String message = "";
+        byte[] data = new byte[2048];
+        int len;
         try {
-          isNewUser = User.newUser(user);
+            len = in.read(data);
+            if (len != -1) {
+                message = new String(data, 0, len, StandardCharsets.UTF_8);
+            }
+            return message;
         } catch (IOException e) {
-          e.printStackTrace();
+            e.printStackTrace();
+            return null;
         }
-        if (isNewUser) {
-          System.out.println("user:" + user);
-          requestURI = "user/reg_successfully.html";
+    }
+
+    private void setRequestLine(String message) {
+        if (message != null) {
+            if (message.toUpperCase().contains("GET") || message.toUpperCase().contains("POST")
+                    || !message.contains("&confirm_password=")) {
+                requestLine = message.substring(0, message.indexOf("\n"));
+                protocol = requestLine.substring(message.indexOf("HTTP"));
+                if (requestLine.toUpperCase().contains("GET")) {
+                    method = "GET";
+                }
+                if (requestLine.toUpperCase().contains("POST")) {
+                    method = "POST";
+                }
+            }
+        }
+    }
+
+    private void parseURL() {
+        int index1, index2;
+        index1 = requestLine.indexOf(' ');
+        if (index1 != -1) {
+            index2 = requestLine.indexOf(' ', index1 + 1);
+            if (index2 > index1) {
+                url = requestLine.substring(index1 + 2, index2);
+            }
+        }
+    }
+
+    private void parseURI() {
+        if (url.contains("?") && url.contains("=")) {
+            String[] strings = url.split("\\?");
+            requestURI = strings[0];
+            queryString = strings[1];
         } else {
-          requestURI = "user/reg_failed.html";
-          System.out.println("NOT Ａ NEW USER");
+            requestURI = url;
         }
-      }
     }
-    System.out.println(requestURI);
-    System.out.println(url);
-  }
 
-  String getMessage() {
-    // 解析uri
-    String message = "";
-    byte[] data = new byte[2048];
-    int len;
-    try {
-      len = in.read(data);
-      if (len != -1) {
-        message = new String(data, 0, len, StandardCharsets.UTF_8);
-      }
-      return message;
-    } catch (IOException e) {
-      e.printStackTrace();
-      return null;
-    }
-  }
-
-  private void setRequestLine(String message) {
-    if (message != null) {
-      if (message.toUpperCase().contains("GET") || message.toUpperCase().contains("POST")
-          || !message.contains("&confirm_password=")) {
-        requestLine = message.substring(0, message.indexOf("\n"));
-        protocol = requestLine.substring(message.indexOf("HTTP"));
-        if (requestLine.toUpperCase().contains("GET")) {
-          method = "GET";
+    private void parseParam() {
+        if (queryString != null) {
+            if (queryString.contains("&")) {
+                String[] strs = queryString.split("&");
+                for (String str : strs) {
+                    String[] params = str.split("=");
+                    parameters.put(URLDecoder.decode(params[0], StandardCharsets.UTF_8),
+                            URLDecoder.decode(params[1], StandardCharsets.UTF_8));
+                }
+            }
         }
-        if (requestLine.toUpperCase().contains("POST")) {
-          method = "POST";
+        System.out.println(parameters);
+    }
+
+    private void storeHeader(String message) {
+        // 拆分http请求头,HashMap存储
+        httpHeader = new HashMap<>();
+        if (message == null) {
+            return;
         }
-      }
-    }
-  }
-
-  private void parseURL() {
-    int index1, index2;
-    index1 = requestLine.indexOf(' ');
-    if (index1 != -1) {
-      index2 = requestLine.indexOf(' ', index1 + 1);
-      if (index2 > index1) {
-        url = requestLine.substring(index1 + 2, index2);
-      }
-    }
-  }
-
-  private void parseURI() {
-    if (url.contains("?") && url.contains("=")) {
-      String[] strings = url.split("\\?");
-      requestURI = strings[0];
-      queryString = strings[1];
-    } else {
-      requestURI = url;
-    }
-  }
-
-  private void parseParam() {
-    HashMap<String, String> query = new HashMap<>();
-    if (queryString != null) {
-      if (queryString.contains("&")) {
-        String[] strs = queryString.split("&");
-        for (String str : strs) {
-          String[] params = str.split("=");
-          query.put(URLDecoder.decode(params[0], StandardCharsets.UTF_8),
-              URLDecoder.decode(params[1], StandardCharsets.UTF_8));
-        }
-      }
-    }
-    System.out.println(query);
-  }
-
-  private void storeHeader(String message) {
-    // 拆分http请求头,HashMap存储
-    httpHeader = new HashMap<>();
-    if (message == null) {
-      return;
-    }
 //    System.out.println(message);
-    if (!message.contains("\r\n")) {
-      return;
+        if (!message.contains("\r\n")) {
+            return;
+        }
+        String subMessage = message.substring(message.indexOf("\r\n")).trim();
+        String[] subMessages = subMessage.split("\\r\\n");
+        for (int i = 0; i < subMessages.length; i++) {
+            if (subMessages[i].contains(": ")) {
+                httpHeader.put(subMessages[i].substring(0, subMessages[i].indexOf(":")),
+                        subMessages[i].substring(subMessages[i].indexOf(":") + 2));
+            }
+        }
     }
-    String subMessage = message.substring(message.indexOf("\r\n")).trim();
-    String[] subMessages = subMessage.split("\\r\\n");
-    for (int i = 0; i < subMessages.length; i++) {
-      if (subMessages[i].contains(": ")) {
-        httpHeader.put(subMessages[i].substring(0, subMessages[i].indexOf(":")),
-            subMessages[i].substring(subMessages[i].indexOf(":") + 2));
-      }
+
+    private User postUser(String message) {
+        // 传送用户
+        if (message.contains("username=") && message.contains("password=")) {
+            message = URLDecoder.decode(message, StandardCharsets.UTF_8);
+            String name = message
+                    .substring(message.indexOf("username=") + 9, message.indexOf("&password="));
+            String passwd;
+            if (message.contains("&confirm_password=")) {
+                passwd = message
+                        .substring(message.indexOf("&password=") + 10, message.indexOf("&confirm_password="));
+            } else {
+                passwd = message.substring(message.indexOf("&password=") + 10);
+            }
+            return new User(name, passwd);
+        }
+        return null;
     }
-  }
 
-  private User postUser(String message) {
-    // 传送用户
-    if (message.contains("username=") && message.contains("password=")) {
-      message = URLDecoder.decode(message, StandardCharsets.UTF_8);
-      String name = message
-          .substring(message.indexOf("username=") + 9, message.indexOf("&password="));
-      String passwd;
-      if (message.contains("&confirm_password=")) {
-        passwd = message
-            .substring(message.indexOf("&password=") + 10, message.indexOf("&confirm_password="));
-      } else {
-        passwd = message.substring(message.indexOf("&password=") + 10);
-      }
-      return new User(name, passwd);
+    String getRequestURI() {
+        return requestURI;
     }
-    return null;
-  }
 
-  String getRequestURI() {
-    return requestURI;
-  }
-
-  String getAcceptType() {
-    return httpHeader.get("Accept");
-  }
+    String getAcceptType() {
+        return httpHeader.get("Accept");
+    }
 }
